557eafff54f20db03d18fafdbcc3f55919566e18
Isaac Larbi <isaacllarbi@gmailcom>
2020-09-06 11:45:08 +0000

    built appbar

---
+++ lib/screens/home_screen/body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(16, 8, 16, 0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -7,26 +7,62 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  TabController _controller;
+  var _userGender = 'WOMEN';
+  var _genderList = ['MEN', 'WOMEN'];
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         key: _scaffoldKey,
-        appBar: _buildAppBar(),
+        appBar: _buildAppBar(context),
         drawer: HomeDrawer(),
       ),
     );
   }
 
-  AppBar _buildAppBar() {
+  _openDrawer() {
+    _scaffoldKey.currentState.openDrawer();
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    var titleTextStyle = TextStyle(
+        fontWeight: FontWeight.bold, color: Colors.black, fontSize: 16);
+    var titleText = Text(_userGender, style: titleTextStyle);
+    var downIcon = Icon(Icons.arrow_drop_down, color: Colors.black);
+    var leadingIcon = Icon(Icons.menu, color: Colors.black);
+
+    var appbarTitle =
+        Row(mainAxisSize: MainAxisSize.min, children: [titleText, downIcon]);
     return AppBar(
-      leading: IconButton(
-        icon: Icon(Icons.menu),
-        onPressed: () => _scaffoldKey.currentState.openDrawer(),
+      leading: IconButton(icon: leadingIcon, onPressed: _openDrawer),
+      title: PopupMenuButton(
+        child: appbarTitle,
+        itemBuilder: (BuildContext context) {
+          return _genderList.map((gender) => buildPopupItem(gender)).toList();
+        },
+        onSelected: (gender) {},
       ),
+      actions: buildActions(context),
+      backgroundColor: Colors.white,
     );
+  }
+
+  PopupMenuItem buildPopupItem(String text) {
+    var popupText = Text(text, style: TextStyle(fontSize: 12));
+    return PopupMenuItem(value: 'text', child: popupText);
+  }
+
+  List<Widget> buildActions(BuildContext context) {
+    var favIcon = Icon(Icons.favorite_border_outlined, color: Colors.black);
+    var searchIcon = Icon(Icons.search, color: Colors.black);
+    return [
+      IconButton(onPressed: () {}, icon: favIcon),
+      IconButton(onPressed: () {}, icon: searchIcon),
+    ];
   }
 }
